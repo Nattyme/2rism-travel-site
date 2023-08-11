8294d1833c1e8004ba33391b400bd72325cffe7c
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2023-08-11 20:59:36 +0300

    JS
    - correction misstypes;

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -1,13 +1,13 @@
 // Mobile nav button
 const navBtn = document.querySelector('.mobile-nav-btn');
-const nav = document.querySelector('mobile-nav');
+const nav = document.querySelector('.mobile-nav');
 const menuIcon = document.querySelector('.nav-icon');
 const fade = document.querySelector('.mobile-nav-fade');
 
 navBtn.onclick = function() {
-    nav.classList.toggle('.mobile-nav--open');
+    nav.classList.toggle('mobile-nav--open');
     fade.classList.toggle('mobile-nav-fade--open');
-    menuIcon.classList.toggle('.nav-icon--active');
+    menuIcon.classList.toggle('nav-icon--active');
     document.body.classList.toggle('no-scroll');
 };
 
